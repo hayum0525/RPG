0ae448c5787f3257f8bd35cf1c9500d966277283
hayum0525 <khyoung9128@gmail.com>
2025-04-09 22:37:06 +0900

    무브먼트.애니메이션 함수 분리, 걷는 애니메이션 추가

--- Assets/02.Scirpt/PlayerMovement.cs
+++ Assets/02.Scirpt/PlayerMovement.cs
@@ -4,23 +4,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movespeed;
-    private Rigidbody charRigidbody;
+    [SerializeField]
+    private float movespeed;
 
-    private void Start()
+    private Animator anim;
+    private bool isWalk = false;
+    private bool isAttack = false;
+
+
+    private void Awake()
     {
-        charRigidbody = GetComponent<Rigidbody>();
-
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
+    {
+
+        playerMovement();
+        Animation();
+
+        
+    }
+
+    void playerMovement()
     {
         float hAxis = Input.GetAxisRaw("Horizontal");
         float vAxis = Input.GetAxisRaw("Vertical");
 
         Vector3 inputDir = new Vector3(hAxis, 0, vAxis).normalized;
 
-        charRigidbody.velocity = inputDir * movespeed;
+
+        transform.position += inputDir * movespeed * Time.deltaTime;
+
         transform.LookAt(transform.position + inputDir);
+        
+    }
+
+    void Animation()
+    {
+        if(isWalk)
+        {
+            anim.SetBool("isWalk", true);
+        }
+        if(isAttack)
+        {
+            anim.SetBool("isAttack", true);
+        }
+        
     }
 }
